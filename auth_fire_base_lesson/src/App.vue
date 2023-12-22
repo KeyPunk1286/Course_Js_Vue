52b6991299c1345c98f362f939d89e7326b0fe12
KeyPunk1286 <107766745+KeyPunk1286@users.noreply.github.com>
2023-12-22 13:16:14 +0200

    Add files via upload

---
+++ auth_fire_base_lesson/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <nav class="navigator">
+            <div class="navigator__router">
+                <router-link to="/">Home</router-link> | <router-link to="/laptop">Laptop</router-link> |
+            </div>
+            <div class="navigator__auth">
+                <user-login />
+            </div>
+        </nav>
+        <router-view />
+    </div>
+</template>
+
+<script>
+// import { mapGetters, mapActions } from 'vuex'
+import UserLogin from '@/components/UserLogin.vue'
+export default {
+    components: {
+        UserLogin,
+    },
+    // computed: {
+    //     ...mapGetters('auth', ['getUser']),
+    // },
+    // methods: {
+    //     ...mapActions('auth', ['loginWithGoogle', 'logout']),
+    // },
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss';
+</style>
